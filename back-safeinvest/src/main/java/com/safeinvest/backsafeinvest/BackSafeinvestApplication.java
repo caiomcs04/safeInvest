8483b919e3f316941824c8be6ceecbc6fd857049
caiomcs04 <caio6c@yahoo.com.br>
2021-11-13 17:01:38 -0300

    back commit

---
+++ back-safeinvest/src/main/java/com/safeinvest/backsafeinvest/BackSafeinvestApplication.java
@@ -0,0 +1,13 @@
+package com.safeinvest.backsafeinvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSafeinvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSafeinvestApplication.class, args);
+	}
+
+}
---
+++ back-safeinvest/src/test/java/com/safeinvest/backsafeinvest/BackSafeinvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safeinvest.backsafeinvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSafeinvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
