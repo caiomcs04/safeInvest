065c623f912779a7d7a36e077d2a3f87cf5d5f5f
caiomcs04 <caio6c@yahoo.com.br>
2021-11-13 16:39:26 -0300

    1#comiit

--- safe-invest-angular/src/app/service/bsscan.service.ts
+++ safe-invest-angular/src/app/service/bsscan.service.ts
@@ -9,10 +9,6 @@ export class UserService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public getAll(rota: any) {
-    return this.httpClient.get("https://api.bscscan.com/api?module=account&action=txlist&address=0xC7d1dFB3f093932A3a4C93BfB2C5737A5977a7A3&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=WMGF8PHKPY3QZS4DH64P1Q962BMTIKR8YS")
-  }
-
   public getListOfBEP20TokenByAddress (address: any) {
     return this.httpClient.get(`https://api.bscscan.com/api?module=account&action=tokentx&address=${address}&offset=5&startblock=0&endblock=999999999&sort=asc&apikey=WMGF8PHKPY3QZS4DH64P1Q962BMTIKR8YS`)
   }
--- safe-invest-angular/src/app/teste/teste.component.ts
+++ safe-invest-angular/src/app/teste/teste.component.ts
@@ -5,33 +5,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-teste',
   templateUrl: './teste.component.html',
-  styleUrls: ['./teste.component.scss']
+  styleUrls: ['./teste.component.scss'],
 })
 export class TesteComponent implements OnInit {
+  endereco: any;
+  transacoes: any;
 
-  endereco:any
-  transacoes:any
-
-  constructor(private userService:UserService,
-    private cmcService:CmcService) { }
+  constructor(
+    private userService: UserService,
+    private cmcService: CmcService
+  ) {}
 
   ngOnInit(): void {
-    this.cmcService.getCMC().subscribe((response:any)=>{
-console.log(response)
-    })
+    this.cmcService.getCMC().subscribe((response: any) => {
+      console.log(response);
+    });
   }
 
-  teste(){
-    this.userService.getListOfBEP20TokenByAddress(this.endereco).subscribe((response:any)=>{
-      this.transacoes = response.result
-      console.log(this.transacoes)
-    })
+  teste() {
+    this.userService
+      .getListOfBEP20TokenByAddress(this.endereco)
+      .subscribe((response: any) => {
+        this.transacoes = response.result;
+        console.log(this.transacoes);
+      });
   }
-cont =0
-  tokenQtd(item:any){
+  cont = 0;
+  tokenQtd(item: any) {
     // item.value = item.value.slice(0,(item.value.length-18))
     // console.log(item.value)
-    console.log(this.cont++)
-  return item.value
+    console.log(this.cont++);
+    return item.value;
   }
 }
