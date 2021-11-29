b99a9ed88f1e6c4eccf2850db8e545daa11feac4
caiomcs04 <caio6c@yahoo.com.br>
2021-11-29 09:05:19 -0300

    ajuste nomes

--- safe-invest-angular/src/app/components/sidebar/sidebar.component.ts
+++ safe-invest-angular/src/app/components/sidebar/sidebar.component.ts
@@ -13,42 +13,42 @@ export const ROUTES: RouteInfo[] = [
     icon: "icon-chart-pie-36",
     class: ""
   },
-  {
-    path: "/icons",
-    title: "Icons",
-    icon: "icon-atom",
-    class: ""
-  },
-  {
-    path: "/maps",
-    title: "Maps",
-    icon: "icon-pin",
-    class: "" },
-  {
-    path: "/notifications",
-    title: "Notifications",
-    icon: "icon-bell-55",
-    class: ""
-  },
+  // {
+  //   path: "/icons",
+  //   title: "Icons",
+  //   icon: "icon-atom",
+  //   class: ""
+  // },
+  // {
+  //   path: "/maps",
+  //   title: "Maps",
+  //   icon: "icon-pin",
+  //   class: "" },
+  // {
+  //   path: "/notifications",
+  //   title: "Notifications",
+  //   icon: "icon-bell-55",
+  //   class: ""
+  // },
 
-  {
-    path: "/user",
-    title: "User Profile",
-    icon: "icon-single-02",
-    class: ""
-  },
-  {
-    path: "/tables",
-    title: "Table List",
-    icon: "icon-puzzle-10",
-    class: ""
-  },
-  {
-    path: "/typography",
-    title: "Typography",
-    icon: "icon-align-center",
-    class: ""
-  },
+  // {
+  //   path: "/user",
+  //   title: "User Profile",
+  //   icon: "icon-single-02",
+  //   class: ""
+  // },
+  // {
+  //   path: "/tables",
+  //   title: "Table List",
+  //   icon: "icon-puzzle-10",
+  //   class: ""
+  // },
+  // {
+  //   path: "/typography",
+  //   title: "Typography",
+  //   icon: "icon-align-center",
+  //   class: ""
+  // },
 ];
 
 @Component({
@@ -59,7 +59,7 @@ export const ROUTES: RouteInfo[] = [
 export class SidebarComponent implements OnInit {
   menuItems: any[];
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
